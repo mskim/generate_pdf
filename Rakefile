47b28cc78b826de0650e0264166a48aa645481cf
Min Soo Kim <mskimsid@gmail.com>
2021-02-03 05:58:29 +0900

    initial commit

---
+++ Rakefile
@@ -0,0 +1,9 @@
+require_relative 'hello_world'
+
+task :default => :gen_pdf
+
+
+desc 'generate pdf'
+task :gen_pdf do
+  system("ruby sample.rb")
+end
---
+++ hello_world.rb
@@ -0,0 +1,26 @@
+# ## Hello World
+#
+# This simple example mimics the classic "hello world" examples from
+# programming languages.
+#
+# Usage:
+# : `ruby hello_world.rb`
+#
+
+require 'hexapdf'
+
+doc = HexaPDF::Document.new
+page = doc.pages.add
+canvas = page.canvas
+puts page.box.width
+puts h = page.box.height
+
+# puts "canvas.box.width:#{canvas.box.width}"
+# puts "canvas.height:#{canvas.height}"
+str = "Hello Min Soo!"
+puts "printing: #{str}"
+canvas.transform(1, 0, 0, 1, 0, h)
+canvas.font('Helvetica', size: 50)
+canvas.text(str, at: [20, -100])
+canvas.text(str, at: [20, -300])
+doc.write("hello_world.pdf", optimize: true)
